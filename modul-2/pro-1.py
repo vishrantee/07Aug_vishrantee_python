c21a452ba794cfc9b7bd3be8085065948dba42ec
vishrantee <patelvishu0983@gmail.com>
2023-08-23 06:28:49 -0700

    modul-2

---
+++ modul-2/pro-1.py
@@ -0,0 +1,7 @@
+a=int(input("Enetr your Number:"))
+if a<0 :
+    print("Your number is negative...")
+elif a>0 :
+    print("Yoru number is positive....")
+else:
+    print("your number is Zero...")
